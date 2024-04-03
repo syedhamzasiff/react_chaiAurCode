f03980ce4a9fc324886e5e18f097fcc0fa38b0ca
syedhamzasiff <syedhamzasiff@gmail.com>
2024-04-03 23:35:57 +0500

    first video

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Hamza</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai Aur React with Vite | Hamza</h1>
+  )
+}
+
+export default App
